d07ce39c88fa0468b1b29e79e19f9e6677186a95
Toma Siklic <toma.siklic@gmail.com>
2022-12-09 08:45:48 +0100

    creating tables Phases and Statuses

--- InternshipDB.sql
+++ InternshipDB.sql
@@ -2,7 +2,7 @@ CREATE TABLE Fields(
 	FieldId SERIAL PRIMARY KEY,
 	Name VARCHAR(30) NOT NULL
 )
-
+--
 CREATE TABLE Interns(
 	InternId SERIAL PRIMARY KEY,
 	FirstName VARCHAR(30),
@@ -11,12 +11,31 @@ CREATE TABLE Interns(
 	DateOfBirth TIMESTAMP,
 	Gender VARCHAR(1) CHECK(Gender IN('M','F'))
 )
-
+--
 CREATE TABLE Members(
 	MemberId SERIAL PRIMARY KEY,
 	FirstName VARCHAR (30),
 	LastName VARCHAR (30),
-	Ping VARCHAR(11) CHECK(LENGTH(Pin) = 11),
+	Pin VARCHAR(11) CHECK(LENGTH(Pin) = 11),
 	DateOfBirth TIMESTAMP,
 	Gender VARCHAR(1) CHECK(Gender IN('M', 'F'))
 )
+
+CREATE TABLE Phases(
+	PhaseId SERIAL PRIMARY KEY,
+	Name VARCHAR(12) CHECK(Name IN('U pripremi','U tijeku', 'Zavrsen'))
+)
+
+CREATE TABLE Statuses(
+	StatusId SERIAL PRIMARY KEY,
+	Name VARCHAR (30) CHECK(Name IN('pripravnik', 'izbacen', 'zavrsen Internship'))
+)
+
+
+
+
+
+
+
+
+
